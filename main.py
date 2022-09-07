c9d30acb76850167d31cc0c02e5ad3713918efd4
Neha <neha.goyal187@gmail.com>
2022-09-07 06:51:48 +0530

    some changes in code

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
-def sum(a,b):
-    s = a+b
-    return sum()
-
-print(sum(6,5))
-print("statement1 in nr")
-print("statement2 in nr")
 def multiply(x,y):
     s = x*y
     return multiply
 
 print(multiply(9,8))
+
+print("statement1 in dv")
